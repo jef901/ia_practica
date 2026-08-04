57e12becd9cf145daddaec3d8df5765bac09007e
Jose Freire <jef901@gmail.com>
2026-08-04 01:11:10 -0300

    Agrega archivos codigo_roto.py e historial_chat.json

---
+++ codigo_roto.py
@@ -0,0 +1,9 @@
+def calcular_promedio(lista_numeros):
+    suma = 0
+    for num in lista_numeros
+        suma = suma + numero  # 'numero' no existe, la variable es 'num'
+    
+    resultado = suma / len(lista_numeros) # Si la lista está vacía dará error
+    return resultado
+
+print(calcular_promedio([]))
